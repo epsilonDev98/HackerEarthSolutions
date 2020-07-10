8a27fcd32d6602c349ff73228bb4accc4f8a643a
sarvesh kumar <sarvesh7617@gmail.com>
2020-07-10 18:21:37 +0530

    Create hello.java

---
+++ Basic input output/hello.java
@@ -0,0 +1,11 @@
+/*
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/hello-32/
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class TestClass {
+    public static void main(String args[] ) throws Exception { 
+        System.out.println("Hello Kirti");
+    }
+}
