a7d0654e9d845a0431a1d80220ab0f411a1bf6c7
sarvesh kumar <sarvesh7617@gmail.com>
2020-07-13 13:51:55 +0530

    Add files via upload

---
+++ ComplexityAnalysis/a+b.java
@@ -0,0 +1,17 @@
+/*
+https://www.hackerearth.com/practice/basic-programming/complexity-analysis/time-and-space-complexity/practice-problems/algorithm/a-b-4/
+*/
+
+import java.util.*;
+import java.math.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+
+        // Write your code here
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+        }
+
+    }
+}
