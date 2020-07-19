5f1988e4d30f18abb0234d2f202e2bb7f36a751a
sarvesh kumar <sarvesh7617@gmail.com>
2020-07-19 11:41:22 +0530

    Create trilogy-b.java
    Trilogy b

---
+++ implementation/trilogy-b.java
@@ -0,0 +1,13 @@
+
+/*
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/gupta-chaturvedi-and-saxena-trilogy-b/
+*/
+
+
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        System.out.println("2232012162");
+
+    }
+}
