c87d18aa2875d7595cf038325bdf94c4fe66983f
sarvesh kumar <sarvesh7617@gmail.com>
2020-07-20 20:14:55 +0530

    Create bob_and_bomb.java

---
+++ implementation/bob_and_bomb.java
@@ -0,0 +1,48 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/bob-and-bombs-cake-walk/
+*/
+
+
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+      
+        Scanner sc=new Scanner(System.in);
+		int tests=sc.nextInt();
+		for(int x=0;x<tests;x++)
+		{
+		    char a[]=sc.next().toCharArray();
+		    int count=0;
+		    for(int i=0;i<a.length;i++)
+		    {
+		        if(a[i]=='B')
+		        {
+		            if((i-1)>=0 && a[i-1]=='W')
+		            {
+		                count++;
+		                a[i-1]='-';
+		            }
+		            if((i-2)>=0 && a[i-2]=='W')
+		            {
+		                count++;
+		                a[i-2]='-';
+		            }
+		            if((i+1)<a.length && a[i+1]=='W')
+		            {
+		                count++;
+		                a[i+1]='-';
+		            }
+		            if((i+2)<a.length && a[i+2]=='W')
+		            {
+		                count++;
+		                a[i+2]='-';
+		            }
+		        }
+		    }
+		    System.out.println(count);
+		}
+
+    }
+}
